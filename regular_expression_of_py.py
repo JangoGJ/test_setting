4c2a859649aa6dd1d806d0e6c94dfde14a046103
JangoGJ <Jango.G@protonmail.com>
2018-07-15 15:04:49 +0800

    0.2

--- regular_expression_of_py.py
+++ regular_expression_of_py.py
@@ -15,10 +15,49 @@ if match:
 #     template = "'{}' does not match pattern '{}'"
 # print(template.format(search_string, pattern))
 
-pattern = "^[a-zA-Z.]+@([a-z.]*\.[a-z]+)$"
-search_string = "some.user@example.com"
-match = re.match(pattern, search_string)
-if match:
-    domain = match.groups()[0]
-    print(domain)
+# pattern = "^[a-zA-Z.]+@([a-z.]*\.[a-z]+)$"
+# search_string = "some.user@example.com"
+# match = re.match(pattern, search_string)
+# if match:
+#     domain = match.groups()[0]
+#     print(domain)
 
+from threading import Timer
+import datetime
+from urllib.request import urlopen
+class UpdatedURL:
+    def __init__(self, url):
+        self.url = url
+        self.contents = ''
+        self.last_updated = None
+        self.update()
+    def update(self):
+        self.contents = urlopen(self.url).read()
+        self.last_updated = datetime.datetime.now()
+        self.schedule()
+    def schedule(self):
+        self.timer = Timer(3600, self.update)
+        self.timer.setDaemon(True)
+        self.timer.start()
+    def __getstate__(self):
+        new_state = self.__dict__.copy()
+        if 'timer' in new_state:
+            del new_state['timer']
+        return new_state
+    def __setstate__(self, data):
+        self.__dict__ = data
+        self.schedule()
+u = UpdatedURL("http://news.yahoo.com/")
+import pickle
+serialized = pickle.dumps(u)
+
+class Contact:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+    @property
+    def full_name(self):
+        return("{} {}".format(self.first, self.last))
+
+c = Contact("John", "Smith")
+json.dumps(c.__dict__)
