0f662416adfb8d839a75d665a7ea2c00ee7bfa0d
JangoGJ <Jango.G@protonmail.com>
2018-07-16 17:16:09 +0800

    0.2

--- interator_sections.py
+++ interator_sections.py
@@ -40,3 +40,42 @@ print(output_integers)
 
 output_ints = [int(n) for n in input_strings if len(n) < 3]
 print(output_ints)
+
+def tally():
+    score = 0
+    while True:
+        increment = yield score
+        score += increment
+white_sox = tally()
+blue_jays = tally()
+next(white_sox)
+
+next(blue_jays)
+
+white_sox.send(3)
+
+blue_jays.send(2)
+
+white_sox.send(2)
+
+blue_jays.send(4)
+
+import re
+def match_regex(filename, regex):
+    with open(filename) as file:
+        lines = file.readlines()
+    for line in reversed(lines):
+        match = re.match(regex, line)
+        if match:
+            regex = yield match.groups()[0]
+def get_serials(filename):
+    ERROR_RE = "XFS ERROR (\[sd[a-z]\])"
+    matcher = match_regex(filename, ERROR_RE)
+    device = next(matcher)
+    while True:
+        bus = matcher.send('(sd \S+) {}.*'.format(re.escape(device)))
+        serial = matcher.send('{} \(SERIAL=([^)]*)\)'.format(bus))
+        yield serial
+        device = matcher.send(ERROR_RE)
+for serial_number in get_serials('EXAMPLE_LOG.log'):
+    print(serial_number)
