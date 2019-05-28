a8184b3fcae755dfe8a9fc337d39711790bccf8d
Jango.G <41154705+JangoGJ@users.noreply.github.com>
2019-05-28 11:18:40 +0800

    Add files via upload

---
+++ 黑域-未成功.py
@@ -0,0 +1,9 @@
+﻿from os import system as cmd_run
+
+File_folder = "C:/Users/XXX/Downloads/platform-tools_r28.0.3-windows/platform-tools/"
+goto_enviroment="C:\Windows\System32\cmd.exe /K "+'"'+"cd /d "+str(File_folder)+'"'
+print(goto_enviroment)
+cmd_run(goto_enviroment)
+print('xxxxxxxxxxxxxxxxxxxx')
+cmd_run('".\adb -d shell sh /data/data/me.piebridge.brevent/brevent.sh"')
+
